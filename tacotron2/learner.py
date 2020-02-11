e23fc2496b79c1f2e3baa8e81b3bd83ca8b04e63
Egor Timofeev <e.timofeev@just-ai.com>
2020-02-11 16:29:06 +0300

    Rsync

--- tacotron2/learner.py
+++ tacotron2/learner.py
@@ -49,9 +49,6 @@ class Learner:
         if self.train_dl.batch_size / n_gpu != int(self.train_dl.batch_size / n_gpu):
             raise ValueError(f"You have {n_gpu} GPUs, batch size must be divisible by {n_gpu}")
 
-        if n_gpu > 1:
-            self.model = torch.nn.DataParallel(self.model)
-
         self.model = self.model.to(self.device)
 
         if fp16_opt_level is not None:
@@ -60,6 +57,9 @@ class Learner:
             except ImportError:
                 raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
             self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level=fp16_opt_level)
+
+        if n_gpu > 1:
+            self.model = torch.nn.DataParallel(self.model)
 
         self.n_epochs = n_epochs
         self.n_epoch_steps = len(self.train_dl)
