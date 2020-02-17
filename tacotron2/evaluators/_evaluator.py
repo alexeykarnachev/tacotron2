6945402f3cc2ddbc1fa969508fcc48f298dd5e9f
Egor Timofeev <e.timofeev@just-ai.com>
2020-02-17 10:38:26 +0300

    Evaluator for embeddings added

--- tacotron2/evaluators/_evaluator.py
+++ tacotron2/evaluators/_evaluator.py
@@ -15,6 +15,28 @@ class BaseEvaluator(object):
 
         self.device = device
 
+    def synthesize(self, text):
+
+        sequence = torch.LongTensor(self.tokenizer.encode(text))\
+                        .unsqueeze(0)\
+                        .to(self.device)
+
+        mel_outputs, mel_outputs_postnet, _, alignments = self.encoder.inference(sequence)
+        audio = self.vocoder.infer(mel_outputs_postnet, sigma=0.9)
+        if self.denoiser:
+            audio = self.denoiser(audio, strength=0.05)[:, 0]
+
+        return audio
+
+
+class EmbeddingEvaluator(BaseEvaluator):
+    """
+    Base evaluator for tacotron + vocoder models pair
+    """
+
+    def __init__(self, encoder, vocoder, tokenizer, denoiser=None, device='cpu'):
+        super(BaseEvaluator).__init__(encoder, vocoder, tokenizer, denoiser, device)
+
     def synthesize(self, text, embedding):
 
         sequence = torch.LongTensor(self.tokenizer.encode(text))\
