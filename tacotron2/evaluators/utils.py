452b5764009db9a5d01c882aa5fcb310dd9cad72
Egor Timofeev <e.timofeev@just-ai.com>
2020-02-17 12:33:38 +0300

    Fix loading of waveglow

--- tacotron2/evaluators/utils.py
+++ tacotron2/evaluators/utils.py
@@ -69,7 +69,7 @@ def get_evaluator(evaluator_classname: str,
 
     vocoder = Factory.get_object(f"waveglow.models.{vocoder_hparams['model_class_name']}", vocoder_hparams)
     vocoder.load_state_dict(
-        torch.load(vocoder_checkpoint_path, map_location=device)
+        torch.load(vocoder_checkpoint_path['model_state_dict'], map_location=device)
     )
 
     if use_denoiser:
