61a085ac4355126cf46033fc75b19d35a3638a3b
Egor Timofeev <e.timofeev@just-ai.com>
2020-02-25 10:28:41 +0300

    Add save best for callback

--- tacotron2/callbacks/model_save_callback.py
+++ tacotron2/callbacks/model_save_callback.py
@@ -17,11 +17,13 @@ class ModelSaveCallback(Callback):
         """
         self.hold_n_models = hold_n_models
         self.models_dir = Path(models_dir)
+        self.best_val_loss = np.inf
 
         self.models_dir.mkdir(exist_ok=False, parents=True)
 
     def on_eval_end(self, learner: Learner):
         self._save(learner)
+        self._save_best(learner)
 
     def on_train_end(self, learner):
         self._save(learner)
@@ -29,6 +31,13 @@ class ModelSaveCallback(Callback):
     def _save(self, learner: Learner):
         self._remove_old_models(models_dir=self.models_dir, hold_n_models=self.hold_n_models)
         self._save_model(learner, models_dir=self.models_dir)
+
+    def _save_best(self, learner: Learner):
+        current_val_loss = learner.valid_loss
+        if np.isfinite(current_val_loss):
+            if current_val_loss < self.best_val_loss:
+                self.best_val_loss = current_val_loss
+                self._save_model(learner, models_dir=self.models_dir, best=True)
 
     @staticmethod
     def _remove_old_models(models_dir: Path, hold_n_models: int):
@@ -43,7 +52,7 @@ class ModelSaveCallback(Callback):
                 file_path.unlink()
 
     @staticmethod
-    def _save_model(learner: Learner, models_dir: Path):
+    def _save_model(learner: Learner, models_dir: Path, best: bool = False):
         save_dict = {
             "model_state_dict": learner.model.state_dict(),
             "optimizer_state_dict": learner.optimizer.state_dict(),
@@ -54,5 +63,9 @@ class ModelSaveCallback(Callback):
             "valid_loss": learner.valid_loss,
             "train_loss": learner.train_loss
         }
-        model_file = models_dir / f'model_{learner.overall_step}.pth'
+        if not best:
+            model_file = models_dir / f'model_{learner.overall_step}.pth'
+        else:
+            model_file = models_dir / 'model_best.pth'
+
         torch.save(save_dict, str(model_file))
