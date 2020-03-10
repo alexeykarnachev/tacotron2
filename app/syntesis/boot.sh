5b056876d4d179ba3f5b60885e784d6bc4f27992
Egor Timofeev <egortimofeev@Egors-MacBook-Pro.local>
2020-03-10 16:35:53 +0300

    Boot fixed

--- app/syntesis/boot.sh
+++ app/syntesis/boot.sh
@@ -3,4 +3,4 @@
 export APP_CONFIG=$1
 PORT=$2
 WORKERS=$3
-exec gunicorn -b :"${PORT}" --timeout 600 -k sync --workers "${WORKERS}" --access-logfile ./gunicorn_access.log --error-logfile ./gunicorn_error.log app:app --log-level DEBUG
+exec gunicorn -b :"${PORT}" --timeout 600 -k sync --workers "${WORKERS}" --access-logfile ./gunicorn_access.log --error-logfile ./gunicorn_error.log app:APP --log-level DEBUG
