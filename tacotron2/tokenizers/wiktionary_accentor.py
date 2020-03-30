e95dce59caec5aebd837166f412af080144cee01
Dasha Serdyuk <d.serdyuk@just-ai.com>
2020-03-30 22:52:44 +0300

    add class wiktionary accentor, slightly change logic

---
+++ tacotron2/tokenizers/wiktionary_accentor.py
@@ -0,0 +1,99 @@
+from typing import List
+from pathlib import Path
+import requests
+import pymorphy2
+from bs4 import BeautifulSoup
+from rnd_utilities.file_utilities import load_json
+import multiprocessing as mp
+from tqdm import tqdm
+
+
+_WIKTIONARY_URL = 'https://ru.wiktionary.org/wiki/Служебная:Поиск?search={}&go=Перейти'
+
+
+class WiktionaryAccentor:
+    def __init__(self, max_n_retries=2):
+        self.max_n_retries = max_n_retries
+        self.word2accents = load_json(Path(__file__).parent / 'data/accents_dict.json')
+        self.morph_analyzer = pymorphy2.MorphAnalyzer()
+
+    def get_accent(self, word, mode):
+        if mode == 'offline':
+            return self._get_accent_offline(word)
+        elif mode == 'online':
+            return self._get_accent_online(word)
+
+    def _get_accent_offline(self, word):
+        index = self.word2accents.get(word, None)
+        if index:
+            word = word[:index] + '+' + word[index:]
+            return word
+        return word
+
+    def _get_accent_online(self, word):
+        parsed = self.morph_analyzer.parse(word)
+        if parsed[0].tag.case != 'nomn':
+            word = parsed[0].normal_form
+
+        variants = self.get_from_wiki(word)
+        not_stressed_variants = [self.delete_stress_sign(v) for v in variants]
+        if word in not_stressed_variants:
+            index = not_stressed_variants.index(word)
+            word = self.stress_sign_to_plus(variants[index])
+        return self.delete_misc_symbols(word)
+
+    def parse_data(self, data: List[str], n_processes=4):
+        with mp.Pool(n_processes) as pool:
+            results = list(tqdm(pool.imap(self.get_from_wiki, data), total=len(data)))
+            pool.close()
+            pool.join()
+        return results
+
+    @staticmethod
+    def get_from_wiki(word, max_n_tries=2):
+        session = requests.Session()
+        session.mount("https://", requests.adapters.HTTPAdapter(max_retries=max_n_tries))
+        headers = {'Content-Type': 'text/html; charset=UTF-8'}
+        try:
+            req = session.get(_WIKTIONARY_URL.format(word), allow_redirects=True, headers=headers)
+        except ConnectionError:
+            print('Connection closed')
+            return []
+        except Exception as e:
+            print('Unexpected error {}'.format(e))
+            return []
+
+        if req.status_code == 200:
+            soup = BeautifulSoup(req.text, 'lxml')
+            try:
+                if 'не существует' in soup.find("b").text:
+                    return []
+                else:
+                    variants = []
+                    for x in soup.find_all('td', {'bgcolor': '#ffffff'}):
+                        if x.br:
+                            x = BeautifulSoup(str(x).replace('<br/>', ' '))
+                        text = x.text.strip()
+                        variants.append(text)
+                    if not variants:
+                        variants = [soup.find("b").text.replace('-', '').lower()]
+                    return variants
+            except AttributeError as e:
+                print(e)
+                return []
+        else:
+            return []
+
+    @staticmethod
+    def delete_misc_symbols(s):
+        s = ''.join([x for x in s if ord(x) != 183])
+        s = s.replace('ѐ', 'е').replace('ѝ', 'и').replace('а̀', 'а')  # delete grave accent
+        return s
+
+    def delete_stress_sign(self, word):
+        word = ''.join([x for x in self.delete_misc_symbols(word) if ord(x) != 769 and ord(x) != 768])
+        return word
+
+    @staticmethod
+    def stress_sign_to_plus(word):
+        return ''.join(['+' if ord(ch) == 769 else ch for ch in word])
--- tests/test_russian_phoneme_tokenizer.py
+++ tests/test_russian_phoneme_tokenizer.py
@@ -1,23 +1,22 @@
-from tacotron2.tokenizers.russian_phoneme_tokenizer import RussianPhonemeTokenizer
+import pytest
 
 
-def test_word_in_wiki():
-    cl = RussianPhonemeTokenizer()
-    result = cl.encode('дезоксирибонуклеиновая')
-    true_value = [2, 13, 40, 110, 4, 46, 79, 40, 75, 40, 8, 4, 62, 98, 46, 55, 41, 62, 4, 102, 4, 44, 4, 3]
-    assert true_value == result
+words = ['дезоксирибонуклеиновая',
+         'игрушка',
+         'абрисах']
+tokens = [[2, 13, 40, 110, 4, 46, 79, 40, 75, 40, 8, 4, 62, 98, 46, 55, 41, 62, 4, 102, 4, 44, 4, 3],
+          [2, 40, 32, 74, 99, 81, 46, 4, 3],
+          [2, 5, 8, 75, 40, 78, 4, 49, 3]]
+test_data = [(words[i], tokens[i]) for i in range(len(words))]
 
 
-def test_word_in_phoneme_dict():
-    cl = RussianPhonemeTokenizer()
-    result = cl.encode('игрушка')
-    true_value = [2, 40, 32, 74, 99, 81, 46, 4, 3]
-    assert true_value == result
+@pytest.fixture
+def russian_phoneme_tokenizer():
+    from tacotron2.tokenizers.russian_phoneme_tokenizer import RussianPhonemeTokenizer
+    return RussianPhonemeTokenizer()
 
 
-def test_word_in_accents_dict():
-    cl = RussianPhonemeTokenizer()
-    result = cl.encode('абрисах')
-    true_value = [2, 5, 8, 75, 40, 78, 4, 49, 3]
-    assert true_value == result
-
+@pytest.mark.parametrize("test_input,expected", test_data)
+def test_word_in_wiki(russian_phoneme_tokenizer, test_input, expected):
+    result = russian_phoneme_tokenizer.encode(test_input)
+    assert expected == result
