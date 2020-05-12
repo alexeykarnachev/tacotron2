0d0fe6f706adecd24054047caed8f35a2f3a99c1
Egor Timofeev <timofeev.egor.perm@gmail.com>
2020-05-12 10:53:51 +0300

    Max decoder step issue frequency reduced

--- tacotron2/models/_modules.py
+++ tacotron2/models/_modules.py
@@ -434,7 +434,6 @@ class Decoder(nn.Module):
         alignments: sequence of attention weights from the decoder
         """
         max_len = self.max_decoder_steps if memory_lengths is None else memory_lengths.max()
-
         decoder_input = self.get_go_frame(memory)
         self.initialize_decoder_states(memory, mask=None)
 
