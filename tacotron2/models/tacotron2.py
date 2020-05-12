6a36552663ebea471d5c6a1aaac420e0d7d28908
Egor Timofeev <timofeev.egor.perm@gmail.com>
2020-05-12 14:58:05 +0300

    No grad removed

--- tacotron2/models/tacotron2.py
+++ tacotron2/models/tacotron2.py
@@ -125,9 +125,9 @@ class Tacotron2KD(nn.Module):
         self.kd_loss_lambda = kd_loss_lambda
 
     def decode(self, encoder_outputs, mels, text_lengths, output_lengths=None):
-        with torch.no_grad():
-            mel_outputs, gate_outputs, alignments = \
-                self.backbone.decode(encoder_outputs, mels, text_lengths)
+        # with torch.no_grad():
+        mel_outputs, gate_outputs, alignments = \
+            self.backbone.decode(encoder_outputs, mels, text_lengths)
         mel_outputs_student, gate_outputs_student, alignments_student = \
             self.student_decoder.inference(encoder_outputs, output_lengths, text_lengths)
         gate_outputs_student = gate_outputs_student.squeeze(2)
@@ -138,8 +138,8 @@ class Tacotron2KD(nn.Module):
         text_inputs, text_lengths, mels, max_len, output_lengths = inputs['x']
         text_lengths, output_lengths = text_lengths.data, output_lengths.data
 
-        with torch.no_grad():
-            encoder_outputs = self.backbone.encode(text_inputs, text_lengths)
+        # with torch.no_grad():
+        encoder_outputs = self.backbone.encode(text_inputs, text_lengths)
 
         (mel_outputs, gate_outputs, alignments), \
         (mel_outputs_student, gate_outputs_student, alignments_student) = self.decode(
